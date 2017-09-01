6313324480413c98cc9e920125cc45b29081fde3
Michal Wiglasz <jsem@michalwiglasz.cz>
2017-09-01 23:05:39 +0200

    Mobile layout tuning

--- lib.php
+++ lib.php
@@ -56,7 +56,7 @@ function print_html_head($root, $description='Denní menu restaurací v okolí')
  ▀█▄▀▪·▀▀▀▀  ▀▀▀ ▀▀▀▀▀•  ▀ ▀▀  █▪▀▀▀▀▀▀·▀▀▀ ▀▀▀ · ▀  ▀ .▀▀▀  ▀▀▀▀ ▀▪▀▀▀·▀▀▀▀ .▀▀▀  ▀  ▀  ▀▀▀▀ ·▀▀▀ • ▀ ·▀▀▀ ·▀▀▀ •
 -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
 
 <meta http-equiv="refresh" content="3600">
 <meta property="og:title" content="Jíííídlooooo">
