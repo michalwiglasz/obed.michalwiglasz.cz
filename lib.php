351f7ab6638d0a6875f2cb5b7c608aa77ec73157
Michal Wiglasz <jsem@michalwiglasz.cz>
2018-03-27 19:42:55 +0200

    Use absolute URL in inline css

--- lib.php
+++ lib.php
@@ -203,7 +203,7 @@ function print_header($restaurant)
 	if ($restaurant->icon) {
 		$id = 'r-' . md5(spl_object_hash($restaurant));
 		echo "<h1 id=\"$id\" class=\"emoji $restaurant->icon\">";
-		echo "<style>h1#$id.emoji.$restaurant->icon:after { background-image: url('em-$restaurant->icon.png'); }</style>";
+		echo "<style>h1#$id.emoji.$restaurant->icon:after { background-image: url('/em-$restaurant->icon.png'); }</style>";
 	}
 	else echo '<h1>';
 	echo escape_text($restaurant->title) . "</h1>\n";
