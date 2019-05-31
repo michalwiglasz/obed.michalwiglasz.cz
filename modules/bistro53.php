f39c61ab199d6084cb8fc126baeb26f9811398a1
Michal Wiglasz <jsem@michalwiglasz.cz>
2019-05-31 19:55:05 +0200

    Dotažení Bistra 53

--- modules/bistro53.php
+++ modules/bistro53.php
@@ -3,31 +3,31 @@
 class Bistro53 extends LunchMenuSource
 {
 	public $title = 'Bistro 53';
-	public $link = '';
-	public $icon = '';
+	public $link = 'https://goo.gl/maps/Z9WC3GeaRJjUDk136';
+	public $sourceLink = 'https://goo.gl/maps/o4ggsLYfQwhRimAo8';
+	public $icon = 'chopsticks';
 
 	public function getTodaysMenu($todayDate, $cacheSourceExpires)
 	{
-	
-		$result->dishes[] = new Dish("Menu jeste neni kompletni...", "", NULL, "Poznamka");
-		$result->dishes[] = new Dish("Pokud poslete fitku menu, doplnime :)", "", NULL, "Poznamka");
+		$result = new LunchMenuResult(DateTime::createFromFormat(DateTime::ATOM, "2019-05-31T19:30:00+0100")->getTimestamp());
 
-		$result->dishes[] = new Dish("PHO GA", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("PHO BO", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("BUN BO NAM BO", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("BUN CHA NEM", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("BUN XAO", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("Pecena Kachna (s prilohou)", "???", NULL, NULL);
-		$result->dishes[] = new Dish("Pecene Kure (s prilohou)", "???", NULL, NULL);
-		$result->dishes[] = new Dish("NEM RAN (zavitky)", "45Kc", NULL, NULL);
-		$result->dishes[] = new Dish("BUN BO NAM BO CHAY", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("PHO XAO", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("TOFU SE ZELENINOU", "99Kc", NULL, NULL);
-		$result->dishes[] = new Dish("Kure na zelenine po thajsku", "99Kc", NULL, NULL);
-		$result->dishes[] = new Dish("Hovezi na zelenine po thajsku", "115Kc", NULL, NULL);
-		$result->dishes[] = new Dish("krevety na zelenine po thajsku", "125Kc", NULL, NULL);
-		
-		
+		$result->dishes[] = new Dish("Tofu se zeleninou (s přílohou)", "99 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Bún bò Nam Bộ chay (tofu, zelenina, bylinky, zálivka)", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Bún bò Nam Bộ (rýžové nudle, zálivka, salát, zelenina, koriandr, restované hovězí maso, arašídy, sušená cibule)", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Bún chả (rýžové nudle, zálivka, salát, zelenina, koriandr, smažené závitky, asijské masové karbanátky)", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Nem rán – smažené asijské závitky (mleté vepřové maso, skleněné nudle, jidášovo ucho, zelenina)", "45 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Bún xào (restované rýžové nudle, kuřecí/vepřové maso, zelenina)", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Phở xào (ploché rýžové nudle, kuřecí/vepřové maso, zelenina)", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Phở bò (hovězí vývar, hovězí maso, ploché rýžové nudle, koriandr a bylinky)", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Phở gà (hovězí vývar, kuřecí maso, ploché rýžové nudle, koriandr a bylinky)", "115 Kč", NULL, NULL);
+
+		$result->dishes[] = new Dish("Hovězí na zelenině / po thajsku", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Krevety na zelenině / po thajsku", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Kuřecí na zelenině / po thajsku", "115 Kč", NULL, NULL);
+
+		$result->dishes[] = new Dish("Pečené kuře s přílohou", "115 Kč", NULL, NULL);
+		$result->dishes[] = new Dish("Pečená kachna s přílohou", "129 Kč", NULL, NULL);
+
 		return $result;
 	}
 }
