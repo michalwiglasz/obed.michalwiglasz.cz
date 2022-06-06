36b69810d50f7612e4fe223048ee6d9e7a5fa38c
Martin Spinler <spinler@cesnet.cz>
2022-06-06 16:50:00 +0200

    Oprava Klubu cestovatelu

--- modules/klubcestovatelu.php
+++ modules/klubcestovatelu.php
@@ -3,7 +3,7 @@
 class KlubCestovatelu extends LunchMenuSource
 {
 	public $title = 'Klub cestovatelů';
-	public $link = 'https://www.hedvabnastezka.cz/klub-cestovatelu-brno/poledni-menu-2/';
+	public $link = 'https://www.klubcestovatelubrno.cz/denni-menu/';
 	public $icon = 'klubcestovatelu';
 
 	public function getTodaysMenu($todayDate, $cacheSourceExpires)
@@ -11,29 +11,31 @@ class KlubCestovatelu extends LunchMenuSource
 		$cached = $this->downloadHtml($cacheSourceExpires);
 		$result = new LunchMenuResult($cached['stored']);
 
+		$content = $cached['html']->find("div.entry-content", 0);
+
 		$regexp = '(' . get_czech_day(date('w', $todayDate)) . '\s+' . date('j', $todayDate) . '\.)ui';
 
 		$pricearr = array();
-		$prices = $cached['html']->find("div.article-content ol li", 0);
+		$prices = $content->find("p strong", 0);
 		if ($prices && !$prices->prev_sibling() && !$prices->next_sibling()) {
 			$prices = explode('/', $prices->plaintext);
 			foreach ($prices as $price) {
-				$price = explode('.', $price);
-				$pricearr[] = trim(ltrim($price[1], " –-"));
+				$price = explode('&#8211;', $price);
+				$pricearr[] = trim(ltrim(str_replace("&nbsp;", "", $price[1]), "–"));
 			}
 		}
 
-		$days = $cached['html']->find("div.article-content p strong span");
+		$days = $content->find("h3");
 		foreach ($days as $day) {
 			if (!preg_match($regexp, $day->plaintext)) continue;
 
-			$today = $day->find_ancestor_tag('p');
-			$soup = $today->next_sibling();
+			$soup = $day->next_sibling();
 			$menu = $soup->next_sibling();
 			while ($menu->tag != "ol" && $menu) {
 				$menu = $menu->next_sibling();
 			}
 
+			$soup = $soup->find("p", 0);
 			if ($soup && $soup->tag == 'p' && trim(str_replace("\xc2\xa0", ' ', html_entity_decode($soup->plaintext))) != NULL) {
 				$result->dishes[] = new Dish(html_entity_decode($soup->plaintext));
 			}
